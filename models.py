bf0881794285e1d3157e52d992d3e4e897790e26
vaidhegi <140682273+Vaidhegi18@users.noreply.github.com>
2024-05-26 13:13:21 +0530

    Add files via upload

---
+++ models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+from django.db import models
+
+class Account(models.Model):
+    email = models.EmailField(unique=True)
+    account_id = models.CharField(max_length=100, unique=True)
+    account_name = models.CharField(max_length=100)
+    app_secret_token = models.CharField(max_length=100, unique=True)
+    website = models.URLField(blank=True)
+
+class Destination(models.Model):
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    url = models.URLField()
+    http_method = models.CharField(max_length=10)  
+    headers = models.JSONField()  
+
